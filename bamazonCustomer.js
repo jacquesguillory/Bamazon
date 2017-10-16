c9a9e67972dc100c9f59d4c83c0e44c308434f7a
Jacques Guillory <jacquesguillory@Jacquess-MacBook-Pro.local>
2017-10-16 16:01:57 -0600

    fixed some DB update logic  and added recursion

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,14 +24,17 @@ customer();
 });
 
 function customer() {
-    // query the database for all items being auctioned
+    // query the database for all items
     connection.query("SELECT * FROM products", function(err, results) {
         if (err) throw err;
 
-        var itemArray = [];
-        for (var i = 0; i < results.length; i++) {
-            itemArray.push(results[i].product_name);
-          }
+       
+       //for testing purposes
+        // var itemArray = [];
+        // for (var i = 0; i < results.length; i++) {
+        //     itemArray.push(results[i].product_name);
+        //   }
+        // console.log(itemArray);
 
         inquirer.prompt([
             {
@@ -41,37 +44,81 @@ function customer() {
 
                   var choiceArray = [];
                   for (var i = 0; i < results.length; i++) {
-                    choiceArray.push(results[i].product_name + ": $" + results[i].price);
-                    itemArray.push(results[i].product_name);
+                    choiceArray.push(results[i].product_name);
 
                   }
                   return choiceArray;
 
                 },
                 message: "Which item would you like to purchase?"
-            },
-            {
-                name: "quantity",
-                type: "input",
-                message: "Please enter the number of items that you wish to buy:"
             }
         ])
         .then(function(answer) {
-            console.log(answer.item);
-            console.log(answer.quantity);
+            // putting chosen item info into a new object
             var chosenItem;
             for (var i = 0; i < results.length; i++) {
                 if (answer.item === results[i].product_name) {
-                  chosenItem = results[i].product_name;
+                    chosenItem = results[i];
                 }
               }
-            console.log(chosenItem);
-            if (parseInt(answer.quantity) < parseInt(results.stock_quantity)) {
-                console.log("Enjoy your new " + answer.item);
-            }
-            else{
-                console.log("Error");
-            }
+            
+            console.log("=====================================================");  
+            console.log("Getting more information on the selected item...");
+            console.log("=====================================================");  
+            console.log("Product Name: " + chosenItem.product_name);
+            console.log("Department: " + chosenItem.department_name);
+            console.log("Price: $" + chosenItem.price); 
+            console.log("Items in Stock: " + chosenItem.stock_quantity);
+            console.log("=====================================================");
+
+
+            inquirer.prompt([
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "Please enter the number of " + chosenItem.product_name + "s that you wish to buy:"
+                }
+            ])
+            .then(function(answer) {
+
+            // Checking if I have enough in stock
+                if (parseInt(answer.quantity) < parseInt(chosenItem.stock_quantity)) {
+
+                    var newStock = chosenItem.stock_quantity - answer.quantity;
+
+                    // updating bamazon_DB
+                    connection.query("UPDATE products SET ? WHERE ?",
+                        [
+                            {
+                                stock_quantity: newStock
+                            },
+                            {
+                                id: chosenItem.id
+                            }
+                        ],
+                        function(error) {
+                            if (error) throw err;
+                            console.log("=====================================================");  
+                            console.log("=====================================================");  
+                            console.log("Product successfully purchased!")
+                            console.log("Enjoy your new " + chosenItem.product_name + "!");
+                            console.log("=====================================================");  
+                            console.log("=====================================================");
+                            console.log("Sending you back to our products page");  
+                            console.log("=====================================================");
+                            customer();
+                        }
+                    );
+
+                    
+                }
+                else{
+                    console.log("Sorry. We do not have enough of that item in our stock to fill your order.");
+                    console.log("Sending you back to our products page");
+                    console.log("=====================================================");
+                    customer();
+                }
+            });   
         });
     });
 }   
